6ce89beec922c9aed7922f3bdc3ed5c1c3fe1b81
garyfishell <garyfishell@192.168.1.156>
2018-12-11 16:06:06 -0800

    First commit

---
+++ FirstGame/src/Main.java
@@ -0,0 +1,26 @@
+import static org.lwjgl.glfw.GLFW.*;
+
+
+
+
+public class Main {
+
+	public static void main(String args[])
+	{
+		if(!glfwInit())
+		{
+			throw new IllegalStateException("GLFW Failed to initialize");
+		}
+		
+		
+		
+		
+		
+		
+		glfwTerminate();
+		
+		
+	}
+	
+	
+}
